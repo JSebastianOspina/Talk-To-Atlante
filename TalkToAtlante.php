6ad4d3bf4be8aaff913968645b3d32ac41ef3793
unknown <sebastianospinacontacto@gmail.com>
2021-11-09 11:10:27 -0500

    Ended array response suport, added examples

--- TalkToAtlante.php
+++ TalkToAtlante.php
@@ -1,59 +1,63 @@
 <?php
+require_once 'ResponseCodes.php';
 
 class TalkToAtlante
 {
     private $errors;
-    private $response;
     private $data;
-    private $statusCode;
 
     /**
-     * @param array $errors The current error from the response.
-     * @param array $response
      * @param array $data The data to be encoded and sent to the client.
-     * @param int $statusCode The last status code sent to client. By default, this is 200
+     * @param array $errors The current error from the response.
      */
-    public function __construct(array $errors = [], $response = '', array $data = [], $statusCode = 200)
+    public function __construct(array $data = [], array $errors = [])
     {
         $this->errors = $errors;
-        $this->response = $response;
         $this->data = $data;
-        $this->statusCode = $statusCode;
     }
 
-    public function setError(ResponseCodes $errorType = null, $message = 'An error ocurred, no details where specified', $statusCode = 400)
+    final public function setError(ResponseCodes $errorType = null, $message = 'An error ocurred, no details where specified', $statusCode = 400)
     {
         if ($errorType !== null) {
             $this->errors[] = [
                 'message' => $errorType->getMessage(),
             ];
-            header('X-PHP-Response-Code: ' . $errorType->getStatusCode(), true, $errorType->getStatusCode());
+            $this->setResponseStatusCode($errorType->getStatusCode());
         } else {
             $this->errors[] = [
                 'message' => $message,
             ];
-            header('X-PHP-Response-Code: ' . $statusCode, true, $statusCode);
+            $this->setResponseStatusCode($statusCode);
         }
-
 
     }
 
-    public function sendResponse()
+    /**
+     * @param $statusCode
+     */
+    private function setResponseStatusCode($statusCode)
+    {
+        header('X-PHP-Response-Code: ' . $statusCode, true, $statusCode);
+    }
+
+    final public function sendResponse(array $data = [], $statusCode = 200)
     {
         $this->enableCors();
         $this->setJsonResponseHeaders();
 
         if (count($this->errors) > 0) {
-            echo json_encode($this->errors);
+            echo json_encode(['errors' => $this->errors]);
             die();
         }
+        $this->setData($data);
         if ($this->data !== null) {
+            $this->setResponseStatusCode($statusCode);
             echo json_encode($this->data);
             die();
         }
     }
 
-    public function enableCors()
+    final public function enableCors()
     {
         //First, check if it is an option request. In this case, the properly cors-headers should be added.
         if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
@@ -66,6 +70,11 @@ class TalkToAtlante
     private function setJsonResponseHeaders()
     {
         header('Content-Type: application/json');
+    }
+
+    private function setData(array $data)
+    {
+        $this->data = $data;
     }
 
     private function formatResponseBody()
--- index.php
+++ index.php
@@ -1,7 +1,48 @@
 <?php
+/*-------------------------------------------- Before using the class ------------------------------------------------*/
+/*All examples start with this symbol 🎈  and ends with another comment , please comment what is under it, and you
+ can skip  to the next example. */
+
+/*Please, always require and create a new TalkToAtlante instance. */
+
 require_once 'TalkToAtlante.php';
-require_once 'ResponseCodes.php';
 $talk = new \TalkToAtlante();
-$talk->setError(null, 'Hola', 500);
+
+/*-------------------------------------------- MANAGING  ERRORS  -----------------------------------------------------*/
+
+/* 🎈 One error example using default error clases 🎈:
+In this case, the custom message  and the status code are ignored becase an error class has already been instantiated */
+
+$talk->setError(new NotFoundError(), 'Ignored message', 500);
+
+/* 🎈 Multiple error example 🎈 :
+In this case, we will create a custom error, but also a default error class will be instantiated.
+In this case both errors will be included in the api response. The http code of the last error code will override the
+other http status codes previously added.*/
+
+$talk->setError(null, 'Error al conectar con la base de datos', 500);
+$talk->setError(new NotFoundError());
+
+/*🎈 Finally, call sendResponse method. It will ignore the data you pass to the method if any error are detected 🎈 */
 $talk->sendResponse();
+
+/*-------------------------------------------- SENDING DATA -----------------------------------------------------*/
+
+//🎈 Single response example. This will output this data as a json response. A 200 status code will be added by default🎈
+$talk->sendResponse([
+    'nombre' => 'Pepito',
+    'correo' => 'email@email.com'
+]);
+
+
+// 🎈 Multiple response usage. In this case we'll send a custom status code as well.  🎈
+$talk->sendResponse([[
+    'nombre' => 'Pepito',
+    'correo' => 'email@email.com'
+], [
+    'nombre' => 'Pepito',
+    'correo' => 'email@email.com'
+]],201);
+
+
 
